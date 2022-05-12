20111733899842c897bfb5694a9be5734bdcf32b
NikosNkl <75068526+NikosNkl@users.noreply.github.com>
2022-05-12 22:24:30 +0300

    Add files via upload

--- Plot_XY.py
+++ Plot_XY.py
@@ -1,50 +1,63 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import dateutil.parser
 from matplotlib.ticker import FormatStrFormatter
 
-ftr = pd.read_csv("data_dsf=0.txt",delim_whitespace=True,usecols = [4,6,10],names = ['col1','col2','col3'])
+df = pd.read_csv("data.txt", usecols=[0, 1, 2, 3], names=['col1', 'col2', 'col3', 'col4'],delim_whitespace=True)  # read exported FreeFlyer epoch text, specify columns picked from .txt, no headers
+
+timestamp = []
+
+a = df['col1']
+b = df['col2']
+c = df['col3']
+d = df['col4']
+
+for i in range (a.size):
+
+    s = "{} 0{} {} {}".format(a.iloc[i], b.iloc[i], c.iloc[i], d.iloc[i]) #read FreeFlyer epoch text as string
+    #s = dateutil.parser.parse(s) #optional : convert to datetime object
+    timestamp.append(s)
+
+print(timestamp[0])
+
+ftr = pd.read_csv("data.txt",delim_whitespace=True,usecols = [4,6,8],names = ['col1','col2','col3']) #FreeFlyer data to visualize
 
 y1 = ftr['col1']
 
-y2 = ftr['col2']/1000
+y2 = ftr['col2']/1000 #manipulate data as needed
 
 y3 = ftr['col3']
 
 fig, ax1 = plt.subplots()
 
-# ax2 = ax1.twinx()
+ax2 = ax1.twinx() #in case a second y-axis needs to be used
 
-# ax1.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+# ax1.yaxis.set_major_formatter(FormatStrFormatter('%.3f')) # define how many decimals appear in values displayed in each axis
 # ax2.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
 
 ax1.set_xlabel('Elapsed Days')
 ax1.set_ylabel('Atmospheric Density - MSIS-2000 (g/cm$^{3}$)')
 # ax1.set_yscale('log')
-# ax2.set_ylabel("In Shadow")
+ax2.set_ylabel("Latitude")
 
-ax1.use_sticky_edges = False
-# ax2.use_sticky_edges = False
-
-ax1.xaxis.get_ticklocs(minor=True)
+ax1.xaxis.get_ticklocs(minor=True) #get minor tick locations
 ax1.yaxis.get_ticklocs(minor=True)
 
-# ax2.xaxis.get_ticklocs(minor=True)
-# ax2.yaxis.get_ticklocs(minor=True)
+ax2.yaxis.get_ticklocs(minor=True)
 
-ax1.set_ylim(0,0.015)
+# ax1.set_ylim(0,0.015) #set axis limits
+# ax1.set_xlim([0, 7])
+
 # ax2.set_ylim([0, 1])
-ax1.set_xlim([0, 7])
-ax1.minorticks_on()
-# ax2.minorticks_on()
+
+ax1.minorticks_on() #activate minor ticks
+ax2.minorticks_on()
 
 plt.title('Satellite, Rho1 = 0')
 
 ax1.plot(y1,y2,linewidth=1)
-# ax2.plot(y1,y3,c='r',linewidth=0.5)
 
-# plt.legend()
-# ax2.legend()
+ax2.plot(y1,y3,c='r',linewidth=0.5)
 
-# plt.tight_layout()
 # plt.grid()
 plt.show()
